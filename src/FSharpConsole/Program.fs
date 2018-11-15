78f7ec4fe7d01f9897b421f891e8acb2cad8f2e2
Jordan Marr <jmarr@microdesk.com>
2018-11-15 18:44:55 -0500

    Initial commit.

---
+++ src/FSharpConsole/Program.fs
@@ -0,0 +1,12 @@
+﻿open CsharpProjWithExt
+
+[<EntryPoint>]
+let main argv = 
+
+    let dir = new System.IO.DirectoryInfo("C:\temp")
+
+    // Intellisense fails when dotting into "dir" fails once "CsharpProjWithExt" is opened.
+    // Commenting out the open statement (or commenting out the extension method) restores intellisense.
+    let parentName = dir.Parent.FullName
+
+    0
