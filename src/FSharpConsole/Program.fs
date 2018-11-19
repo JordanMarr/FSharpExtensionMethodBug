b2392a622c287f787606c63286dd59b0771ddde0
Jordan Marr <jmarr@microdesk.com>
2018-11-19 12:16:25 -0500

    improved bug description in comment

--- src/FSharpConsole/Program.fs
+++ src/FSharpConsole/Program.fs
@@ -5,8 +5,13 @@ let main argv =
 
     let dir = new System.IO.DirectoryInfo("C:\temp")
 
-    // Intellisense fails when dotting into "dir" fails once "CsharpProjWithExt" is opened.
-    // Commenting out the open statement (or commenting out the extension method) restores intellisense.
+    // Intellisense fails when dotting into "dir" when the "CsharpProjWithExt" is opened above.
+    // Note that the this project does not reference the System.Windows.Forms assembly,
+    // but the imported extension method takes a "System.Windows.Forms.Form" argument.
+    // Workarounds to restore intellisense include:
+    // 1) Commenting out the "open CsharpProjWithExt" statement
+    // 2) Commenting out the extension method
+    // 3) Referencing the "System.Windows.Forms" assembly in the F# project.
     let parentName = dir.Parent.FullName
 
     0
